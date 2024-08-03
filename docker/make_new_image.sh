7bcc7d9434f3aa8568bf954ac2ff0fc47c3438af
Piotr <piotrciosmak2001@gmail.com>
2024-08-03 14:54:53 +0200

    refactor

--- docker/make_new_image.sh
+++ docker/make_new_image.sh
@@ -3,6 +3,7 @@
 [[ ! -f "docker/make_new_image.sh" ]] && echo "ERROR: Script must be run from main project directory" && exit 1
 
 [[ "$#" -ne 2 ]] && echo "ERROR: Usage: $0 <IMAGE_VERSION> <ARCHITECTURE (x64 or arm64)>" && exit 1
+
 IMAGE_VERSION="$1"
 IFS='.' read -r MAJOR MINOR PATCH <<< "$IMAGE_VERSION"
 ! [[ $MAJOR =~ ^[0-9]+$ ]] || ! [[ $MINOR =~ ^[0-9]+$ ]] || ! [[ $PATCH =~ ^[0-9]+$ ]] && echo "ERROR: <IMAGE_VERSION> must be in format NUMBER.NUMBER.NUMBER" && exit 1
