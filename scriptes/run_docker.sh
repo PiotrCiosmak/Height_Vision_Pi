8a865492362ee96144886e93e6d134be1cc2c299
Piotr <piotrciosmak2001@gmail.com>
2024-08-04 20:08:09 +0200

    updated

--- scriptes/run_docker.sh
+++ scriptes/run_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ARCHITECTURE=${1:arm64}
 VERSION=${2:0.1.0}
-docker run --network host -it --device=/dev/video0 -v $(pwd):/Height_Vision_Pi --entrypoint /bin/bash ghcr.io/piotrciosmak/height-vision-pi/"$ARCHITECTURE":"$VERSION"
+docker run --network host -it -v /run/udev:/run/udev -v $(pwd):/Height_Vision_Pi --privileged --entrypoint /bin/bash ghcr.io/piotrciosmak/height-vision-pi/"$ARCHITECTURE":"$VERSION"
