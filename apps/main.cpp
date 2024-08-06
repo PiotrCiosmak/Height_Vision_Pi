89184854a41fcb30ee4131e551f8244778a5ba73
Piotr <piotrciosmak2001@gmail.com>
2024-08-07 00:55:57 +0200

    refactor

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <libcamera/camera_manager.h>
 
 int main()
 {
-    return 0;
+    auto camera_manager{std::make_unique<libcamera::CameraManager>()};
+    if (camera_manager->start())
+    {
+        std::cerr << "ERROR: Failed to start camera manager" << std::endl;
+        exit(1);
+    }
+
+    auto cameras = camera_manager->cameras();
+    if (cameras.empty())
+    {
+        std::cerr << "ERROR: No cameras found" << std::endl;
+        exit(1);
+    }
+
+
+
+    camera_manager->stop();
 }
