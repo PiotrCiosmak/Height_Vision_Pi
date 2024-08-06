1c01d11467f8b76fc53b357745627156883481ae
Piotr <piotrciosmak2001@gmail.com>
2024-08-07 00:24:08 +0200

    test

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,27 +1,34 @@
-// Opencv needs to be installed on your Raspberry Pi
 #include <opencv2/opencv.hpp>
 using namespace std;
 using namespace cv;
 
 int main() {
 
-    VideoCapture cap(0,CAP_V4L2 );
+    VideoCapture cap(0, CAP_V4L2);
     if (!cap.isOpened()) {
         cout << "Cannot open camera\n";
         return -1;
     }
 
     Mat frame;
+    namedWindow("Camera Feed", WINDOW_AUTOSIZE); // Tworzymy okno do wyświetlania obrazu
+
     while (true) {
         bool ret = cap.read(frame);
         if (!ret) {
-            cout << "Error. Fail to receive frame.\n";
+            cout << "Error. Failed to receive frame.\n";
             break;
         }
-        if (waitKey(1) == 'q') {
+
+        imshow("Camera Feed", frame); // Wyświetlamy klatkę w oknie
+
+        if (waitKey(1) == 'q') { // Sprawdzamy, czy naciśnięto 'q'
             break;
         }
     }
+
+    cap.release(); // Zwolnienie zasobów kamery
+    destroyAllWindows(); // Zamknięcie wszystkich okien OpenCV
 
     return 0;
 }
