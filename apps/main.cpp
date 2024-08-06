c4e88d148ecd3495c6030aba7503dcd9676ad2be
Piotr <piotrciosmak2001@gmail.com>
2024-08-07 00:53:43 +0200

    revted

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,32 +1,6 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
-#include <libcamera/formats.h>
 
-using namespace std;
-using namespace cv;
-
-int main() {
-    // Inicjalizacja obiektu VideoCapture
-    VideoCapture cap(0, libcamera::formats::NV12); // Używa domyślnego backendu dla kamery
-
-    if (!cap.isOpened()) {
-        cout << "Cannot open camera\n";
-        return -1;
-    }
-
-    Mat frame;
-    if (cap.read(frame)) { // Próbuj odczytać jedną klatkę
-        // Zapisz klatkę do pliku
-        bool success = imwrite("captured_frame.jpg", frame); // Zapisuje obraz do pliku "captured_frame.jpg"
-        if (!success) {
-            cout << "Error: Failed to save the frame.\n";
-        } else {
-            cout << "Frame saved as 'captured_frame.jpg'.\n";
-        }
-    } else {
-        cout << "Error: Failed to capture frame.\n";
-    }
-
-    cap.release(); // Zwolnienie zasobów kamery
+int main()
+{
     return 0;
 }
