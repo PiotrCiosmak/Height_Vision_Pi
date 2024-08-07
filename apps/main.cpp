01d462902291b3e406cdad3e43c75f78dbf57190
Piotr <piotrciosmak2001@gmail.com>
2024-08-07 21:47:00 +0200

    opencv test

--- apps/main.cpp
+++ apps/main.cpp
@@ -106,7 +106,7 @@ static void processRequest(Request *request)
 		 * must be mapped by the application
 		 */
 
-		cv::Mat image(1280,720, CV_8UC1, buffer);
+		cv::Mat image(1280,720, CV_8UC1, &buffer[0]);
 
 		// Save the image using OpenCV
 		std::string filename = "image_" + std::to_string(metadata.sequence) + ".png";
