7bc56ce5a810629353adc98618f6399bb71d7273
Piotr <piotrciosmak2001@gmail.com>
2024-08-09 01:10:58 +0200

    test

--- apps/main.cpp
+++ apps/main.cpp
@@ -299,9 +299,10 @@ int main()
 	/*
 	 * The Camera configuration procedure fails with invalid parameters.
 	 */
+	streamConfig.size.width = 1280; //4096
+	streamConfig.size.height = 720; //2560
 #if 0
-	streamConfig.size.width = 0; //4096
-	streamConfig.size.height = 0; //2560
+
 
 	int ret = camera->configure(config.get());
 	if (ret) {
