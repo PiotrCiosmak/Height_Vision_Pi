f466b4827f32d5a95f37565384610d3a4bd9c589
Piotr <piotrciosmak2001@gmail.com>
2024-08-14 22:10:45 +0200

    test

--- apps/main.cpp
+++ apps/main.cpp
@@ -7,29 +7,22 @@ int main()
 {
     cv::Mat frame;
     cv::VideoCapture cap(cv::CAP_LIBCAMERA);
-    cap.set(cv::CAP_PROP_FRAME_WIDTH, 2312); //Width selection, is auto adjusted for supported values
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 1736); //Height Selection
-    cap.set(cv::CAP_PROP_FPS, 30);
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, 4096); //Width selection, is auto adjusted for supported values
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 3074); //Height Selection
     cap.set(cv::CAP_PROP_MODE, 1); //PixelFormat Selection
     cap.set(cv::CAP_PROP_FORMAT, 0); //StreamRole Selection
-
 
     std::string a = cap.getBackendName();
     cout << "Backend: " << a << std::endl;
 
-    if (cap.isOpened() == true)
-    {
+    if (cap.isOpened() == true) {
         cout << "\nTrue" << std::endl;
-    }
-    else
-    {
+    } else {
         cout << "False";
     }
 
-    while (true)
-    {
-        if (cap.read(frame))
-        {
+    while (true) {
+        if (cap.read(frame)) {
             imshow("Original Video", frame);
             int width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
             int height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
