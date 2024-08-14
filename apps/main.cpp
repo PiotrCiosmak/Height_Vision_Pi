1cf4e1f38faef30d11f09caedf9777c8ea065c1a
Piotr <piotrciosmak2001@gmail.com>
2024-08-14 22:09:12 +0200

    test

--- apps/main.cpp
+++ apps/main.cpp
@@ -10,20 +10,8 @@ int main()
     cap.set(cv::CAP_PROP_FRAME_WIDTH, 2312); //Width selection, is auto adjusted for supported values
     cap.set(cv::CAP_PROP_FRAME_HEIGHT, 1736); //Height Selection
     cap.set(cv::CAP_PROP_FPS, 30);
-    cap.set(cv::CAP_PROP_MODE, true); //PixelFormat Selection
+    cap.set(cv::CAP_PROP_MODE, 1); //PixelFormat Selection
     //cap.set(cv::CAP_PROP_FORMAT, 0); //StreamRole Selection
-    auto brightness = cap.get(cv::CAP_PROP_BRIGHTNESS);
-    std::cout << "brightness:" << brightness << std::endl;
-    auto contrast = cap.get(cv::CAP_PROP_CONTRAST);
-    std::cout << "contrast:" << contrast << std::endl;
-    auto saturation = cap.get(cv::CAP_PROP_SATURATION);
-    std::cout << "saturation:" << saturation << std::endl;
-    auto hue = cap.get(cv::CAP_PROP_HUE);
-    std::cout << "hue:" << hue << std::endl;
-    auto gain = cap.get(cv::CAP_PROP_GAIN);
-    std::cout << "gain:" << gain << std::endl;
-    auto exposure = cap.get(cv::CAP_PROP_EXPOSURE);
-    std::cout << "exposure:" << exposure << std::endl;
 
 
     std::string a = cap.getBackendName();
