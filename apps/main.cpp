181de9c1991d2c3fbff9416062fd15be6b70bdfc
Piotr <piotrciosmak2001@gmail.com>
2024-08-14 22:10:01 +0200

    test

--- apps/main.cpp
+++ apps/main.cpp
@@ -11,7 +11,7 @@ int main()
     cap.set(cv::CAP_PROP_FRAME_HEIGHT, 1736); //Height Selection
     cap.set(cv::CAP_PROP_FPS, 30);
     cap.set(cv::CAP_PROP_MODE, 1); //PixelFormat Selection
-    //cap.set(cv::CAP_PROP_FORMAT, 0); //StreamRole Selection
+    cap.set(cv::CAP_PROP_FORMAT, 0); //StreamRole Selection
 
 
     std::string a = cap.getBackendName();
