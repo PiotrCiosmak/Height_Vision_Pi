ecf917309302ed286ea40293b822055bc37560cf
Piotr <piotrciosmak2001@gmail.com>
2024-08-17 23:09:33 +0200

    test

--- apps/main.cpp
+++ apps/main.cpp
@@ -17,7 +17,7 @@ int main()
 //TODO 1. Make DummyCameraController
 //TODO 2. Test for CameraController
 
-#include <iostream>	#include <camera/ArduCamCameraController.hpp>
+#include <iostream>
 #include <opencv2/opencv.hpp>
 
 using namespace std;
@@ -32,15 +32,10 @@ int main()
     cap.set(cv::CAP_PROP_FPS, 30); //PixelFormat Selection
     cap.set(cv::CAP_PROP_AUTOFOCUS, true); //PixelFormat Selection
     cap.set(cv::CAP_PROP_AUTO_EXPOSURE, true); //PixelFormat Selection
-    std::string a = cap.getBackendName();
-    cout << "Backend: " << a << std::endl;
-    if (cap.isOpened() == true)
+
+    if (!cap.isOpened() == true)
     {
-        cout << "\nTrue" << std::endl;
-    }
-    else
-    {
-        cout << "False";
+        cout << "\nFalse" << std::endl;
     }
 
     while (true)
@@ -48,14 +43,6 @@ int main()
         if (cap.read(frame))
         {
             imshow("Original Video", frame);
-            int width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
-            int height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
-
-            if (cv::waitKey(1) == 'q') // Press 'q' to exit the loop
-            {
-                break;
-            }
         }
     }
-    return 0;
 }
