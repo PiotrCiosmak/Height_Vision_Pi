e06d905483a82942d4312de36b02490c38e4d8e4
Piotr <piotrciosmak2001@gmail.com>
2024-08-18 00:01:42 +0200

    test

--- include/camera/ArduCamCameraController.hpp
+++ include/camera/ArduCamCameraController.hpp
@@ -11,6 +11,6 @@ namespace height_vision_pi
         [[nodiscard]] auto getFrame() -> cv::Mat override;
 
     private:
-       // cv::VideoCapture video_capture;
+        cv::VideoCapture video_capture;
     };
 }
--- src/camera/ArduCamCameraController.cpp
+++ src/camera/ArduCamCameraController.cpp
@@ -3,10 +3,10 @@
 using namespace height_vision_pi;
 
 ArduCamCameraController::ArduCamCameraController(const CameraConfig& new_camera_config) :
-    CameraController(new_camera_config)//,
-   // video_capture(cv::CAP_LIBCAMERA)
+    CameraController(new_camera_config),
+    video_capture(cv::CAP_LIBCAMERA)
 {
-    /*video_capture.set(cv::CAP_PROP_FRAME_WIDTH, camera_config.resolution.x);
+    video_capture.set(cv::CAP_PROP_FRAME_WIDTH, camera_config.resolution.x);
     video_capture.set(cv::CAP_PROP_FRAME_HEIGHT, camera_config.resolution.y);
     video_capture.set(cv::CAP_PROP_FPS, camera_config.fps);
     video_capture.set(cv::CAP_PROP_MODE, camera_config.capture_mode);
@@ -19,23 +19,11 @@ ArduCamCameraController::ArduCamCameraController(const CameraConfig& new_camera_
     if (!video_capture.isOpened())
     {
         std::cerr << "ERROR: Camera isn't working" << std::endl;
-    }*/
+    }
 }
 
 auto ArduCamCameraController::getFrame() -> cv::Mat
 {
-    cv::Mat frame;
-    cv::VideoCapture video_capture(cv::CAP_LIBCAMERA);
-    video_capture.set(cv::CAP_PROP_FRAME_WIDTH, camera_config.resolution.x);
-    video_capture.set(cv::CAP_PROP_FRAME_HEIGHT, camera_config.resolution.y);
-    video_capture.set(cv::CAP_PROP_FPS, camera_config.fps);
-    video_capture.set(cv::CAP_PROP_MODE, camera_config.capture_mode);
-    //video_capture.set(cv::CAP_PROP_BRIGHTNESS, camera_config.brightness);
-    //video_capture.set(cv::CAP_PROP_SATURATION, camera_config.saturation);
-    //video_capture.set(cv::CAP_PROP_HUE, camera_config.hue);
-    //video_capture.set(cv::CAP_PROP_GAIN, camera_config.gain);
-    video_capture.set(cv::CAP_PROP_AUTOFOCUS, camera_config.auto_focus);
-    video_capture.set(cv::CAP_PROP_AUTO_EXPOSURE, camera_config.auto_exposure);
     if (!video_capture.isOpened() == true)
     {
         std::cout << "\nFalse" << std::endl;
