568f63097fa07883a4898c56f78d59e2bb4c1800
Piotr <piotrciosmak2001@gmail.com>
2024-08-18 00:15:55 +0200

    test

--- src/camera/ArduCamCameraController.cpp
+++ src/camera/ArduCamCameraController.cpp
@@ -25,13 +25,28 @@ ArduCamCameraController::ArduCamCameraController(const CameraConfig& new_camera_
 void ArduCamCameraController::getFrame(cv::Mat& frame)
 {
     cv::Mat frame;
+    cv::VideoCapture cap(cv::CAP_LIBCAMERA);
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, 2048); //Width selection, is auto adjusted for supported values
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 1537); //Height Selection
+    cap.set(cv::CAP_PROP_MODE, true); //PixelFormat Selection
+    cap.set(cv::CAP_PROP_FPS, 30); //PixelFormat Selection
+    cap.set(cv::CAP_PROP_AUTOFOCUS, true); //PixelFormat Selection
+    cap.set(cv::CAP_PROP_AUTO_EXPOSURE, true); //PixelFormat Selection
+    if (!cap.isOpened() == true)
+    {
+        std::cout << "\nFalse" << std::endl;
+    }
+
     while (true)
     {
-        video_capture.read(frame);
-        imshow("dasda", frame);
-        if (cv::waitKey(1) == 'q') // Press 'q' to exit the loop
+        if (cap.read(frame))
         {
-            break;
+            imshow("Original Video", frame);
+
+            if (cv::waitKey(1) == 'q') // Press 'q' to exit the loop
+            {
+                break;
+            }
         }
     }
 }
