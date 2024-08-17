998b836d4cdf74734a436d99ba6c91cd61e24941
Piotr <piotrciosmak2001@gmail.com>
2024-08-18 00:34:47 +0200

    test

--- src/camera/ArduCamCameraController.cpp
+++ src/camera/ArduCamCameraController.cpp
@@ -25,16 +25,18 @@ ArduCamCameraController::ArduCamCameraController(const CameraConfig& new_camera_
 
 void ArduCamCameraController::getFrame(cv::Mat& frame)
 {
-    video_capture.read(frame);
-    imshow("Original Video", frame);
+    if(video_capture.read(frame))
+    {
+        imshow("Original Video", frame);
+    }
 
     //TODO IMSHOW POZA
-    if (!video_capture.read(frame))
+    /*if (!video_capture.read(frame))
     {
         std::cerr << "ERROR: Can't capture frame" << std::endl;
     }
     if (frame.empty())
     {
         std::cerr << "ERROR: Captured frame is empty" << std::endl;
-    }
+    }*/
 }
