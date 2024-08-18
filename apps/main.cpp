547a3bf38e928ad52eed694fe57b17b053a9a9e3
Piotr <piotrciosmak2001@gmail.com>
2024-08-18 11:55:40 +0200

    test

--- apps/main.cpp
+++ apps/main.cpp
@@ -12,7 +12,10 @@ int main()
     while (true)
     {
         auto frame = camera_controller->getFrame();
-        //imshow(window_config.name, frame);
+        if(!frame.empty())
+        {
+            imshow("window_config.name", frame);
+        }
         //cv::resizeWindow(window_config.name, window_config.resolution.x, window_config.resolution.y);
         if (cv::waitKey(1) == 'q')
         {
