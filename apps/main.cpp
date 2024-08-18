6befc1c538576f8febc4acbd13fb7e9ad8ac91d4
Piotr <piotrciosmak2001@gmail.com>
2024-08-18 11:57:03 +0200

    test

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,22 +1,23 @@
+#include "camera/ArduCamCameraController.hpp"
+#include "config/WindowConfig.hpp"
+
 #include <thread>
 
-#include "camera/ArduCamCameraController.hpp"
-//#include "config/WindowConfig.hpp"
 
 using namespace height_vision_pi;
 
 int main()
 {
     const auto camera_controller = std::make_unique<ArduCamCameraController>(CameraConfig{});
-    //const auto window_config = WindowConfig{};
+    const auto window_config = WindowConfig{};
     while (true)
     {
         auto frame = camera_controller->getFrame();
-        if(!frame.empty())
+        if (!frame.empty())
         {
-            imshow("window_config.name", frame);
+            imshow(window_config.name, frame);
+            cv::resizeWindow(window_config.name, window_config.resolution.x, window_config.resolution.y);
         }
-        //cv::resizeWindow(window_config.name, window_config.resolution.x, window_config.resolution.y);
         if (cv::waitKey(1) == 'q')
         {
             break;
