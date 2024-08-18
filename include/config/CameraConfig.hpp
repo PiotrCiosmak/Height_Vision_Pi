448e69a294e8d881e784e831474ef82baacf70a1
Piotr <piotrciosmak2001@gmail.com>
2024-08-18 12:33:26 +0200

    fps 10

--- include/config/CameraConfig.hpp
+++ include/config/CameraConfig.hpp
@@ -7,7 +7,7 @@ namespace height_vision_pi
     struct CameraConfig
     {
         Resolution resolution{2048, 1537};
-        int fps = 20;
+        int fps = 10;
         bool capture_mode = true;
         double brightness = 0.5;
         double contrast = 0.5;
