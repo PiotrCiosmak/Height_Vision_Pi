7082f42b6472d81cef0ca06649b96bde63353030
Piotr <piotrciosmak2001@gmail.com>
2024-08-18 11:52:20 +0200

    test

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,3 +1,5 @@
+#include <thread>
+
 #include "camera/ArduCamCameraController.hpp"
 //#include "config/WindowConfig.hpp"
 
@@ -7,14 +9,15 @@ int main()
 {
     const auto camera_controller = std::make_unique<ArduCamCameraController>(CameraConfig{});
     //const auto window_config = WindowConfig{};
-    //while (true)
-    //{
+    while (true)
+    {
         auto frame = camera_controller->getFrame();
         //imshow(window_config.name, frame);
         //cv::resizeWindow(window_config.name, window_config.resolution.x, window_config.resolution.y);
-        /*if (cv::waitKey(1) == 'q')
+        if (cv::waitKey(1) == 'q')
         {
             break;
-        }*/
-    //}
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
 }
