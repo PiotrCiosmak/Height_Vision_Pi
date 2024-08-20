93de34a481793535f502f0f3ca1c742de548d375
Piotr <piotrciosmak2001@gmail.com>
2024-08-20 10:55:32 +0200

    injectors for camera controller done

---
+++ include/Injector.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "camera/CameraController.hpp"
+
+#include <boost/di.hpp>
+
+namespace height_vision_pi
+{
+    template <typename T>
+    using UniqueInjector = boost::di::injector<std::unique_ptr<T>>;
+    [[nodiscard]] auto cameraControllerInjector() -> UniqueInjector<CameraController>;
+    [[nodiscard]] auto dummyCameraControllerInjector() -> UniqueInjector<CameraController>;
+    [[nodiscard]] auto arduCamCameraControllerInjector() -> UniqueInjector<CameraController>;
+}
