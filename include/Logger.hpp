692c82b6dca99fdd24dc39314b68a78fd1df58da
Piotr <piotrciosmak2001@gmail.com>
2024-08-27 19:42:54 +0200

    logger added

--- include/Logger.hpp
+++ include/Logger.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <mutex>
+#include <iostream>
 #include <sstream>
 
 namespace height_vision_pi
@@ -16,23 +16,59 @@ namespace height_vision_pi
         };
 
         template<typename... Args>
-        static void log(Level level, const std::string& format, Args... args);
+        static void log(Level level, const std::string& format, Args... args)
+        {
+            std::ostringstream oss;
+            formatMessage(oss, format, args...);
 
-        template<typename... Args> static void info(const std::string& format, Args... args);
+            switch (level)
+            {
+            case Level::INFO:
+                std::cout << "[INFO] " << oss.str() << '\n';
+                break;
+            case Level::WARN:
+                std::cout << "[WARN] " << oss.str() << '\n';
+                break;
+            case Level::ERROR:
+                std::cerr << "[ERROR] " << oss.str() << '\n';
+                std::exit(EXIT_FAILURE);
+            }
+        }
 
-        template<typename... Args> static void warn(const std::string& format, Args... args);
+        template<typename... Args> static void info(const std::string& format, Args... args)
+        {
+            log(Level::INFO, format, args...);
+        }
 
-        template<typename... Args> static void error(const std::string& format, Args... args);
+        template<typename... Args> static void warn(const std::string& format, Args... args)
+        {
+            log(Level::WARN, format, args...);
+        }
+
+        template<typename... Args> static void error(const std::string& format, Args... args)
+        {
+            log(Level::ERROR, format, args...);
+        }
 
     private:
         template<typename T, typename... Args>
         static void formatMessage(std::ostringstream& oss,
                                   const std::string& format,
                                   T value,
-                                  Args... args);
-        static void formatMessage(std::ostringstream& oss, const std::string& format);
-        static void exit_thread_safe();
+                                  Args... args)
+        {
+            auto pos = format.find("{}");
+            if (pos != std::string::npos)
+            {
+                oss << format.substr(0, pos) << value;
+                formatMessage(oss, format.substr(pos + 2), args...);
+            }
+            else
+            {
+                oss << format;
+            }
+        }
 
-        static std::mutex exit_mutex;
+        static void formatMessage(std::ostringstream& oss, const std::string& format);
     };
 } // namespace height_vision_pi
