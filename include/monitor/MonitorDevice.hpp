ad1bad2e636400713edbd66756246e306fc52fd8
Piotr <piotrciosmak2001@gmail.com>
2024-09-03 23:44:56 +0200

    more method in interface

--- include/monitor/MonitorDevice.hpp
+++ include/monitor/MonitorDevice.hpp
@@ -13,6 +13,11 @@ namespace height_vision_pi
         }
 
         virtual void checkCPUTemperature() = 0;
+        virtual void checkCPUUsage() = 0;
+        virtual void checkDiskUsage() = 0;
+        virtual void checkRAMUsage() = 0;
+        virtual void checkGPUUsage() = 0;
+
         virtual ~MonitorDevice() = default;
 
     protected:
