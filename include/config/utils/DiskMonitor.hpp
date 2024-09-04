f7cbda400b0b05a667c6950adf1af65b9e112a30
Piotr <piotrciosmak2001@gmail.com>
2024-09-04 22:01:02 +0200

    config added

---
+++ include/config/utils/DiskMonitor.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <nlohmann/json.hpp>
+
+namespace height_vision_pi
+{
+    struct DiskMonitor
+    {
+        std::string path;
+        double warning_usage_percentage;
+        double error_usage_percentage;
+
+        NLOHMANN_DEFINE_TYPE_INTRUSIVE(DiskMonitor,
+                                       path,
+                                       warning_usage_percentage,
+                                       error_usage_percentage)
+    };
+} // namespace height_vision_pi
