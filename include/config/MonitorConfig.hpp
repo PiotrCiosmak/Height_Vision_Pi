8952b9c7d262694fb7fee891da7c0b85d8ba8333
Piotr <piotrciosmak2001@gmail.com>
2024-09-04 22:02:36 +0200

    disk monitor config added

--- include/config/MonitorConfig.hpp
+++ include/config/MonitorConfig.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
 #include "config/utils/CPUMonitor.hpp"
+#include "config/utils/DiskMonitorConfig.hpp"
 
 namespace height_vision_pi
 {
     struct MonitorConfig
     {
         CPUMonitor cpu_monitor;
+        DiskMonitorConfig disk_monitor;
 
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE(MonitorConfig, cpu_monitor)
+        NLOHMANN_DEFINE_TYPE_INTRUSIVE(MonitorConfig, cpu_monitor, disk_monitor)
     };
 } // namespace height_vision_pi
--- include/config/utils/DiskMonitorConfig.hpp
+++ include/config/utils/DiskMonitorConfig.hpp
@@ -4,13 +4,13 @@
 
 namespace height_vision_pi
 {
-    struct DiskMonitor
+    struct DiskMonitorConfig
     {
         std::string path;
         double warning_usage_percentage;
         double error_usage_percentage;
 
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE(DiskMonitor,
+        NLOHMANN_DEFINE_TYPE_INTRUSIVE(DiskMonitorConfig,
                                        path,
                                        warning_usage_percentage,
                                        error_usage_percentage)
