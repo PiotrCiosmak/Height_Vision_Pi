e79f9e891e76dac83b33fa61b3f3461822abf247
Piotr <piotrciosmak2001@gmail.com>
2024-09-04 22:01:26 +0200

    checkDiskUsage implemented

--- include/monitor/Monitor.hpp
+++ include/monitor/Monitor.hpp
@@ -9,8 +9,18 @@ namespace height_vision_pi
     public:
         explicit Monitor(const MonitorConfig& new_monitor_config);
         void checkCPUTemperature() override;
+        void checkCPUUsage() override;
+        void checkDiskUsage() override;
+        void checkRAMUsage() override;
+        void checkGPUUsage() override;
 
     private:
         [[nodiscard]] auto getCPUTemperature() -> float;
+        [[nodiscard]] auto getCPUUsage() -> float;
+        [[nodiscard]] auto getDiskUsage() -> int;
+        [[nodiscard]] auto getDiskStatistics(const std::string& all_disk_statistics) -> std::string;
+        [[nodiscard]] auto getDiskPercentageUsage(const std::string& disk_statistics) -> int;
+        [[nodiscard]] auto getRAMUsage() -> float;
+        [[nodiscard]] auto getGPUUsage() -> float;
     };
 } // namespace height_vision_pi
--- src/monitor/Monitor.cpp
+++ src/monitor/Monitor.cpp
@@ -1,5 +1,6 @@
 #include "monitor/Monitor.hpp"
 #include "Logger.hpp"
+#include "monitor/utils/BashCommand.hpp"
 
 #include <fstream>
 
@@ -42,10 +43,104 @@ auto Monitor::getCPUTemperature() -> float
         return 0;
     }
 
-    std::string temperature;
+    auto temperature = std::string{};
     std::getline(file, temperature);
     file.close();
 
     constexpr auto millicelsius_to_celsius = 1000.0F;
     return std::stof(temperature) / millicelsius_to_celsius;
+}
+
+void Monitor::checkCPUUsage()
+{
+    // TODO
+}
+
+auto Monitor::getCPUUsage() -> float
+{
+    // TODO
+}
+
+void Monitor::checkDiskUsage()
+{
+    const auto usage = getDiskUsage();
+    const auto warning_usage = monitor_config.disk_monitor.warning_usage_percentage;
+    const auto error_usage = monitor_config.disk_monitor.error_usage_percentage;
+
+    if (usage >= error_usage)
+    {
+        Logger::error("Disk usage is {} % , which exceeds the critical threshold of {} %. The "
+                      "application will now shut down.",
+                      usage,
+                      error_usage);
+    }
+    else if (usage >= warning_usage)
+    {
+        Logger::warn("Disk usage is {} % , which exceeds the warning threshold of {} %.",
+                     usage,
+                     error_usage);
+    }
+}
+
+auto Monitor::getDiskUsage() -> int
+{
+    const auto all_disk_statistics = runBashCommand("df").value();
+    const auto disk_statistics = getDiskStatistics(all_disk_statistics);
+    const auto disk_usage = getDiskPercentageUsage(disk_statistics);
+    return disk_usage;
+}
+
+auto Monitor::getDiskStatistics(const std::string& all_disk_statistics) -> std::string
+{
+    auto stream = std::istringstream{all_disk_statistics};
+    auto line = std::string{};
+
+    while (std::getline(stream, line))
+    {
+        if (line.find(monitor_config.disk_monitor.path))
+        {
+            return line;
+        }
+    }
+
+    Logger::error("Disk {} not found", monitor_config.disk_monitor.path);
+}
+
+auto Monitor::getDiskPercentageUsage(const std::string& disk_statistics) -> int
+{
+    const auto percent_position = disk_statistics.find('%');
+
+    if (percent_position != std::string::npos)
+    {
+        const auto space_position = disk_statistics.rfind(' ', percent_position);
+
+        if (space_position != std::string::npos)
+        {
+            const auto number_str =
+                disk_statistics.substr(space_position + 1, percent_position - space_position - 1);
+            return std::stoi(number_str);
+        }
+    }
+
+    Logger::error("Disk {} percentage usage not found", disk_statistics);
+}
+
+void Monitor::checkRAMUsage()
+{
+    // TODO
+}
+
+auto Monitor::getRAMUsage() -> float
+{
+    // TODO
+}
+
+void Monitor::checkGPUUsage()
+{
+    // TODO
+}
+
+auto Monitor::getGPUUsage() -> float
+{
+    // TODO
 }
