bdc13489bbf53aa00c647f1df18717145667adea
Piotr <piotrciosmak2001@gmail.com>
2024-09-10 00:51:15 +0200

    opty

--- src/detector/HumanDetector.cpp
+++ src/detector/HumanDetector.cpp
@@ -10,27 +10,25 @@ HumanDetector::HumanDetector()
 
 auto HumanDetector::detect(cv::Mat& frame) -> cv::Mat&
 {
-    auto outputLayerNames = net.getUnconnectedOutLayersNames();
+    // TODO poprawić to ale jest potencjał! :)
     auto blob = cv::Mat{};
     cv::dnn::blobFromImage(
-        frame, blob, 1.0 / 255.0, cv::Size(96, 96), cv::Scalar(0, 0, 0), true, false); // Zmiana rozmiaru obrazu na 96x96
+        frame, blob, 1.0 / 255.0, cv::Size(128, 128), cv::Scalar(0, 0, 0), true, false);
     net.setInput(blob);
-
     auto detections = std::vector<cv::Mat>{};
-    net.forward(detections, outputLayerNames); // Użycie wstępnie zapisanych nazw warstw wyjściowych
+    net.forward(detections, net.getUnconnectedOutLayersNames());
 
     auto indices = std::vector<int>{};
     auto boxes = std::vector<cv::Rect>{};
     auto confidences = std::vector<float>{};
 
-#pragma omp parallel for // Równoległe przetwarzanie detekcji
     for (const auto& detection : detections)
     {
         for (int i = 0; i < detection.rows; ++i)
         {
             const auto* data = detection.ptr<float>(i);
             auto confidence = data[4];
-            if (confidence > 0.9) // próg pewności
+            if (confidence > 0.98) // próg pewności
             {
                 auto center_x = static_cast<int>(data[0] * frame.cols);
                 auto center_y = static_cast<int>(data[1] * frame.rows);
@@ -40,16 +38,13 @@ auto HumanDetector::detect(cv::Mat& frame) -> cv::Mat&
                 auto left = center_x - width / 2;
                 auto top = center_y - height / 2;
 
-#pragma omp critical // Synchronizacja dla dostępu do wektorów
-                {
-                    boxes.push_back(cv::Rect(left, top, width, height));
-                    confidences.push_back(confidence);
-                }
+                boxes.push_back(cv::Rect(left, top, width, height));
+                confidences.push_back(confidence);
             }
         }
     }
 
-    cv::dnn::NMSBoxes(boxes, confidences, 0.9, 0.4, indices); // Optymalizacja NMS
+    cv::dnn::NMSBoxes(boxes, confidences, 0.9, 0.1, indices);
 
     for (int idx : indices)
     {
