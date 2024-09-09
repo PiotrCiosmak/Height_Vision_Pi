8e5fc41bcba2d3bc5c0d33017869cd3ed766c7d3
Piotr <piotrciosmak2001@gmail.com>
2024-09-09 23:49:14 +0200

    test

--- src/detector/HumanDetector.cpp
+++ src/detector/HumanDetector.cpp
@@ -4,35 +4,51 @@ using namespace height_vision_pi;
 
 HumanDetector::HumanDetector()
 {
-    //TODO path to config file
+    // TODO path to config file
     net = cv::dnn::readNetFromDarknet("../models/yolov4.cfg", "../models/yolov4.weights");
 }
 
 auto HumanDetector::detect(cv::Mat& frame) -> cv::Mat&
 {
-    cv::Mat blob;
-    cv::dnn::blobFromImage(frame, blob, 1.0 / 255.0, cv::Size(416, 416), cv::Scalar(0, 0, 0), true, false);
-    net.setInput(blob);  // Ensure that `net` is not const
-    std::vector<cv::Mat> outs;
-    net.forward(outs, net.getUnconnectedOutLayersNames());
+    // TODO poprawić to ale jest potencjał! :)
+    auto blob = cv::Mat{};
+    cv::dnn::blobFromImage(
+        frame, blob, 1.0 / 255.0, cv::Size(416, 416), cv::Scalar(0, 0, 0), true, false);
+    net.setInput(blob);
+    auto detections = std::vector<cv::Mat>{};
+    net.forward(detections, net.getUnconnectedOutLayersNames());
 
-    for (const auto& out : outs)
+    auto indices = std::vector<int>{};
+    auto boxes = std::vector<cv::Rect>{};
+    auto confidences = std::vector<float>{};
+
+    for (const auto& detection : detections)
     {
-        for (int i = 0; i < out.rows; ++i)
+        for (int i = 0; i < detection.rows; ++i)
         {
-            const auto* data = out.ptr<float>(i);
-            float confidence = data[4];
-            if (confidence > 0.5)
+            const auto* data = detection.ptr<float>(i);
+            auto confidence = data[4];
+            if (confidence > 0.5) // próg pewności
             {
-                int centerX = static_cast<int>(data[0] * frame.cols);
-                int centerY = static_cast<int>(data[1] * frame.rows);
-                int width = static_cast<int>(data[2] * frame.cols);
-                int height = static_cast<int>(data[3] * frame.rows);
+                auto center_x = static_cast<int>(data[0] * frame.cols);
+                auto center_y = static_cast<int>(data[1] * frame.rows);
+                auto width = static_cast<int>(data[2] * frame.cols);
+                auto height = static_cast<int>(data[3] * frame.rows);
 
-                cv::Rect bbox(centerX - width / 2, centerY - height / 2, width, height);
-                cv::rectangle(frame, bbox, cv::Scalar(0, 255, 0), 2);
+                auto left = center_x - width / 2;
+                auto top = center_y - height / 2;
+
+                boxes.push_back(cv::Rect(left, top, width, height));
+                confidences.push_back(confidence);
             }
         }
+    }
+
+    cv::dnn::NMSBoxes(boxes, confidences, 0.5, 0.4, indices);
+
+    for (int idx : indices)
+    {
+        cv::rectangle(frame, boxes[idx], cv::Scalar(0, 255, 0), 2);
     }
 
     return frame;
