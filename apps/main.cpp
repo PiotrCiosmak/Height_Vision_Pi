d20a28b9836b02300136816ad10d42b03d161421
Piotr <piotrciosmak2001@gmail.com>
2024-11-16 19:57:25 +0100

    refactor done

--- apps/main.cpp
+++ apps/main.cpp
@@ -7,18 +7,25 @@
 
 using namespace height_vision_pi;
 
+namespace
+{
+    void saveFrame(const cv::Mat& frame);
+#ifdef AARCH64
+    void showFrame(const cv::Mat& frame);
+#endif
+    void waitForNextFrame(const std::chrono::high_resolution_clock::time_point& start_time);
+} // namespace
+
 int main()
 {
-    const std::chrono::milliseconds frame_duration{1000
-                                                   / static_cast<int>(Config::get().camera.fps)};
 #ifdef AARCH64
     const auto monitor = monitorInjector().create<std::unique_ptr<MonitorDevice>>();
 #endif
     const auto camera_controller =
         cameraControllerInjector().create<std::unique_ptr<CameraController>>();
-
     const auto human_detector = humanDetectorInjector().create<std::unique_ptr<HumanDetector>>();
     const auto face_detector = faceDetectorInjector().create<std::unique_ptr<FaceDetector>>();
+
     while (true)
     {
         const auto start_time = std::chrono::high_resolution_clock::now();
@@ -34,22 +41,44 @@ int main()
             // TODO Calculate the distance between the pupils
             // TODO Calculate height based on age and distance between the pupils
 #ifdef AARCH64
-            imshow(Config::get().window.name, frame);
+            showFrame();
 #else
-            static auto frame_counter = 0;
-            std::ostringstream filename;
-            filename << "frame_" << std::setw(4) << std::setfill('0') << frame_counter++ << ".png";
-            cv::imwrite(filename.str(), frame);
+            saveFrame(frame);
 #endif
-            cv::resizeWindow(Config::get().window.name,
-                             Config::get().camera.resolution.x,
-                             Config::get().camera.resolution.y);
         }
         if (cv::waitKey(1) == 'q')
         {
             break;
         }
 
+        waitForNextFrame(start_time);
+    }
+}
+
+namespace
+{
+    void saveFrame(const cv::Mat& frame)
+    {
+        static auto frame_counter = 0;
+        std::ostringstream filename;
+        filename << "frame_" << std::setw(4) << std::setfill('0') << frame_counter++ << ".png";
+        cv::imwrite(filename.str(), frame);
+    }
+
+#ifdef AARCH64
+    void showFrame(const cv::Mat& frame)
+    {
+        imshow(Config::get().window.name, frame);
+        cv::resizeWindow(Config::get().window.name,
+                         Config::get().camera.resolution.x,
+                         Config::get().camera.resolution.y);
+    }
+#endif
+
+    void waitForNextFrame(const std::chrono::high_resolution_clock::time_point& start_time)
+    {
+        const auto frame_duration =
+            std::chrono::milliseconds{1000 / static_cast<int>(Config::get().camera.fps)};
         const auto end_time = std::chrono::high_resolution_clock::now();
         const auto elapsed_time = end_time - start_time;
         const auto sleep_duration =
@@ -60,4 +89,4 @@ int main()
             std::this_thread::sleep_for(sleep_duration);
         }
     }
-}
+} // namespace
