ff52326d7f6e19786e04fd4616b451fa89e8185b
Piotr <piotrciosmak2001@gmail.com>
2024-11-17 16:21:54 +0100

    revert

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -13,43 +13,4 @@ IFS='.' read -r MAJOR MINOR PATCH <<< "$VERSION"
 
 sudo xhost +Local:*
 
-docker run --network host -it \
--v /run/udev:/run/udev \
--v $(pwd):/Height_Vision_Pi \
---device /dev/video0:/dev/video0 \
---device /dev/video1:/dev/video1 \
---device /dev/video2:/dev/video2 \
---device /dev/video3:/dev/video3 \
---device /dev/video4:/dev/video4 \
---device /dev/video5:/dev/video5 \
---device /dev/video6:/dev/video6 \
---device /dev/video7:/dev/video7 \
---device /dev/video19:/dev/video19 \
---device /dev/video20:/dev/video20 \
---device /dev/video21:/dev/video21 \
---device /dev/video22:/dev/video22 \
---device /dev/video23:/dev/video23 \
---device /dev/video24:/dev/video24 \
---device /dev/video25:/dev/video25 \
---device /dev/video26:/dev/video26 \
---device /dev/video27:/dev/video27 \
---device /dev/video28:/dev/video28 \
---device /dev/video29:/dev/video29 \
---device /dev/video30:/dev/video30 \
---device /dev/video31:/dev/video31 \
---device /dev/video32:/dev/video32 \
---device /dev/video33:/dev/video33 \
---device /dev/video34:/dev/video34 \
---device /dev/video35:/dev/video35 \
---device /dev/video36:/dev/video36 \
---device /dev/video37:/dev/video37 \
---device /dev/media0:/dev/media0 \
---device /dev/media1:/dev/media1 \
---device /dev/media2:/dev/media2 \
---device /dev/media3:/dev/media3 \
---device /dev/v4l-subdev0:/dev/v4l-subdev0 \
---device /dev/v4l-subdev1:/dev/v4l-subdev1 \
---device /dev/v4l-subdev2:/dev/v4l-subdev2 \
---device /dev/v4l-subdev3:/dev/v4l-subdev3 \
--e DISPLAY=${DISPLAY} \
---entrypoint /bin/bash ghcr.io/piotrciosmak/height-vision-pi/$ARCHITECTURE:$VERSION
+docker run --network host -it -v /run/udev:/run/udev -v $(pwd):/Height_Vision_Pi --privileged -e DISPLAY=${DISPLAY} --entrypoint /bin/bash ghcr.io/piotrciosmak/height-vision-pi/$ARCHITECTURE:$VERSION
