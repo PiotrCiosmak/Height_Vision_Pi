994b90835c38d9dca1e3fcceaf3908652917d5be
Piotr <piotrciosmak2001@gmail.com>
2024-11-17 16:01:01 +0100

    media0 mapped

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -13,4 +13,4 @@ IFS='.' read -r MAJOR MINOR PATCH <<< "$VERSION"
 
 sudo xhost +Local:*
 
-docker run --network host -it -v /run/udev:/run/udev -v $(pwd):/Height_Vision_Pi --device /dev/video0:/dev/video0 -e DISPLAY=${DISPLAY} --entrypoint /bin/bash ghcr.io/piotrciosmak/height-vision-pi/$ARCHITECTURE:$VERSION
+docker run --network host -it -v /run/udev:/run/udev -v $(pwd):/Height_Vision_Pi --device /dev/video0:/dev/video0 --device /dev/media0:/dev/media0 -e DISPLAY=${DISPLAY} --entrypoint /bin/bash ghcr.io/piotrciosmak/height-vision-pi/$ARCHITECTURE:$VERSION
