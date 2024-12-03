21f752c7d684a69914cc8c9b1953b81c081ec6fd
Piotr <piotrciosmak2001@gmail.com>
2024-12-03 23:04:25 +0100

    new Age Detector config added

--- include/config/AgeDetectorConfig.hpp
+++ include/config/AgeDetectorConfig.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "utils/ResolutionConfig.hpp"
+#include "utils/ColorConfig.hpp"
+
 #include <nlohmann/json.hpp>
 
 namespace height_vision_pi
@@ -8,9 +11,19 @@ namespace height_vision_pi
     {
         std::string model_path;
         std::string proto_path;
+        double scale_factor;
+        ResolutionConfig resolution;
+        ColorConfig color;
+        bool swap_red_and_blue;
+        bool crop;
 
         NLOHMANN_DEFINE_TYPE_INTRUSIVE(AgeDetectorConfig,
                                        model_path,
-                                       proto_path)
+                                       proto_path,
+                                       scale_factor,
+                                       resolution,
+                                       color,
+                                       swap_red_and_blue,
+                                       crop)
     };
 } // namespace height_vision_pi
