6b261dcff8d313cc15e738b7b33c307d8ce7e71e
Piotr <piotrciosmak2001@gmail.com>
2025-10-07 20:20:59 +0200

    Test added Logs to HeightCalculator added

--- src/calculator/HeightCalculator.cpp
+++ src/calculator/HeightCalculator.cpp
@@ -4,6 +4,11 @@
 
 using namespace height_vision_pi;
 
+inline bool isHeightInValidRange(const double height)
+{
+    return height >= 60 && height <= 250;
+}
+
 auto HeightCalculator::calculate(
     const std::vector<cv::Mat>& humans,
     const std::vector<cv::Mat>& faces,
@@ -52,8 +57,15 @@ auto HeightCalculator::calculate(
 
         const auto height = human.rows * cm_per_px;
 
-        heights.emplace_back(height);
+        if (isHeightInValidRange(height))
+        {
+            heights.emplace_back(height);
+        }
     }
+
+    Logger::info("{} out of {} possible heights calculated",
+                 heights.size(),
+                 size);
 
     return heights;
 }
---
+++ tests/functional/FullFlowTest.cpp
@@ -0,0 +1,80 @@
+#include "Injector.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace height_vision_pi;
+
+class FullFlowTest : public ::testing::Test
+{
+protected:
+    std::unique_ptr<CameraController> camera_controller;
+    std::unique_ptr<HumanDetector> human_detector;
+    std::unique_ptr<FaceDetector> face_detector;
+    std::unique_ptr<AgeDetector> age_detector;
+    std::unique_ptr<PupilsDistanceCalculator> pupils_distance_calculator;
+    std::unique_ptr<HeightCalculator> height_calculator;
+
+    void SetUp() override
+    {
+        camera_controller = cameraControllerInjector().create<std::unique_ptr<CameraController>>();
+        human_detector = humanDetectorInjector().create<std::unique_ptr<HumanDetector>>();
+        face_detector = faceDetectorInjector().create<std::unique_ptr<FaceDetector>>();
+        age_detector = ageDetectorInjector().create<std::unique_ptr<AgeDetector>>();
+        pupils_distance_calculator = pupilsDistanceCalculatorInjector().create<std::unique_ptr<
+            PupilsDistanceCalculator>>();
+        height_calculator = heightCalculatorInjector().create<std::unique_ptr<HeightCalculator>>();
+    }
+};
+
+TEST_F(FullFlowTest, FullFlowTest)
+{
+    // given: Path to a directory that contains human frames
+    const auto directory_path = std::string{
+        std::string{PROJECT_SOURCE_DIR} + "/resources/detected_human"};
+    // given: Placeholder for a currently processed frame
+    auto frame = std::vector<cv::Mat>{};
+    for (const auto& entry : std::filesystem::directory_iterator(directory_path))
+    {
+        const auto img = cv::imread(entry.path().string());
+        if (!img.empty())
+        {
+            frame.emplace_back(img);
+        }
+    }
+
+    // then: Frame isn't empty
+    ASSERT_FALSE(frame.empty());
+
+    // when: Detecting humans from one random frame
+    const auto& detected_humans = human_detector->detect(frame.at(34));
+
+    // then: One human should be detected
+    ASSERT_TRUE(detected_humans.size() == 1);
+
+    // when: Detecting faces
+    const auto& detected_faces = face_detector->detect(detected_humans);
+
+    // then: One face should be detected
+    ASSERT_TRUE(detected_faces.size() == 1);
+
+    // when: Detecting ages
+    const auto& detected_ages = age_detector->detect(detected_faces);
+
+    // then: Ages should be detected
+    ASSERT_TRUE(detected_ages.size() == 1);
+
+    // when: Calculating pupils
+    const auto& pupils_distances = pupils_distance_calculator->calculate(detected_faces);
+
+    // then: Pupils distance should be calculated
+    ASSERT_TRUE(pupils_distances.size() == 1);
+
+    // when: Calculating height
+    const auto& heights = height_calculator->calculate(detected_humans,
+                                                       detected_faces,
+                                                       detected_ages,
+                                                       pupils_distances);
+
+    // then: Height should be calculated correctly
+    ASSERT_TRUE(heights.size() == 1);
+}
